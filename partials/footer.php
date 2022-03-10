5544090b13a0b24db0ae18c10a1548422498c71c
Tkacz <gabrielmt2@al.insper.edu.br>
2022-03-10 08:42:02 -0300

    att

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,8 @@
 <hr>
 
-<center><span>Scandiweb Test Assignment</span></center>
+<center>
+    <span>Scandiweb Test Assignment</span><br>
+    <a href="https://github.com/gtkacz/scandiweb-ps" target="_blank">
+         <img alt="Github repo" src="img/github.png" width="35" height="35" class="repo">
+    </a>
+</center>
