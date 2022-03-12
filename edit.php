48599ca0fc5a67dd0a4b21ef4f9afeffd180db42
Tkacz <gabrielmt2@al.insper.edu.br>
2022-03-12 14:45:35 -0300

    att

--- edit.php
+++ edit.php
@@ -28,12 +28,14 @@ if(isset($_GET["SKU"], $_GET["type"])){
             <button type="button" class="btn btn-danger btn-size" onclick="window.location.href='index.php'">Cancel</button>
             </div>
         </div>
-        <div class="card hover-overlay hover-zoom hover-shadow ripple">
-            <span><?php echo $product->SKU ?></span>
-            <span><?php echo $product->NAME ?></span>
-            <span><?php echo $product->PRICE ?></span>
+        <center>
+        <div class="card-edit">
+            <input type="text" class="fake-input" id="sku" name="SKU" value=<?php echo $product->SKU ?> oninvalid="this.setCustomValidity('Please, submit required data')" oninput="this.setCustomValidity('')" required><br>
+            <input type="text" class="fake-input" id="name" name="Name" value=<?php echo $product->NAME ?> oninvalid="this.setCustomValidity('Please, submit required data')" oninput="this.setCustomValidity('')" required><br>
+            <input type="text" class="fake-input" pattern="[0-9]+" id="price" name="Price" value=<?php echo $product->PRICE ?> oninvalid="this.setCustomValidity('Please, submit required data')" oninput="this.setCustomValidity('')" required><br>
             <span><?php echo $product->productAttribute ?></span>
         </div>
+        </center>
         </form>
         <?php include ("partials/footer.php"); ?>
     </div>
